76495ee1ef4bdecbc28a523510126c37672a03bb
prastutiii <prastutiacharya123@gmail.com>
2025-11-19 22:21:32 +0545

    Week 4 updates

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Veluxe.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult AdminProduct()
+        {
+            return View();
+        }
+
+        public IActionResult AdminOrder()
+        {
+            return View();
+        }
+
+        public IActionResult AdminUser()
+        {
+            return View();
+        }
+    }
+}
+
